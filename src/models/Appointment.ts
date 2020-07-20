ae95467fff42867848f814aaf4ee812ff1cec455
PAC <PAC@NBPAC>
2020-07-20 12:19:18 -0300

    Criando model de agendamento

--- src/models/Appointment.ts
+++ src/models/Appointment.ts
@@ -1,17 +1,22 @@
-import { uuid } from 'uuidv4';
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
+@Entity('appointments')
 class Appointment {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column('varchar')
   provider: string;
 
+  @Column('time with time zone')
   date: Date;
 
-  constructor({ provider, date }: Omit<Appointment, 'id'>) {
-    this.id = uuid();
-    this.provider = provider;
-    this.date = date;
-  }
+  //Não é mais necessario gerar construtor, pois o Entity já gera o constructor
+  // constructor({ provider, date }: Omit<Appointment, 'id'>) {
+  //   this.id = uuid();
+  //   this.provider = provider;
+  //   this.date = date;
+  // }
 }
 
 export default Appointment;
